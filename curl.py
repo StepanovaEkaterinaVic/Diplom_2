96ba1bbef5eaf88db04d562515a3ef95c61c468c
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-05-23 12:32:54 +0300

    Создан файл curl.py с необходимыми URL

---
+++ curl.py
@@ -0,0 +1,6 @@
+class URL:
+    BASE_URL = 'https://stellarburgers.nomoreparties.site'
+    CREATE_USER_URL = f'{BASE_URL}/api/auth/register'
+    LOGIN_USER_URL = f'{BASE_URL}/api/auth/login'
+    USER_URL = f'{BASE_URL}/api/auth/user'
+    ORDER_URL = f'{BASE_URL}/api/orders'
